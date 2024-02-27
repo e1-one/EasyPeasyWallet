c04b1235ac12f7b7c54dc5bf0d9d80a03ea60d74
e1-one <vanya.yarovoy@gmail.com>
2024-02-26 20:15:58 -0500

    100% mutation coverage. Also, fixes to the code was made.

--- src/main/java/com/easypeasy/wallet/exchange/services/WalletOperationsImpl.java
+++ src/main/java/com/easypeasy/wallet/exchange/services/WalletOperationsImpl.java
@@ -20,53 +20,66 @@ public class WalletOperationsImpl implements WalletOperations {
         this.currencyExchange = currencyExchange;
     }
     public void withdraw(String userId, double amountToWithdraw, Currency withdrawalCurrency, long walletId) {
-        //1. changed conditional boundary → SURVIVED
-        //2. Substituted 0.0 with 1.0 → SURVIVED
-        //4. removed conditional - replaced comparison check with false → SURVIVED
+        //1. changed conditional boundary → KILLED
+        //2. Substituted 0.0 with 1.0 → KILLED
+        //4. removed conditional - replaced comparison check with false → KILLED
+        // NOTE: more test cases were added.
         if (amountToWithdraw <= 0)
             throw new IllegalArgumentException("Amount to withdraw must be greater than zero");
         UserProfile userProfile = userDao.getUserProfile(userId);
         if (!userProfile.wallets().contains(walletId))
             throw new IllegalArgumentException("Wallet " + walletId + " must belong to the user " + userId);
         Wallet wallet = walletDao.getWallet(userId);
-        //1. changed conditional boundary → SURVIVED
+        //1. changed conditional boundary → KILLED
+        // more test cases were added.
         if(wallet.balance() < amountToWithdraw)
             throw new IllegalStateException("Not enough funds on balance. " + walletId);
-        BigDecimal withdrawalAmountInWalletCurrency
-        //1. removed call to com/easypeasy/wallet/exchange/model/Wallet::currency → SURVIVED
-                = convertToCurrency(amountToWithdraw, wallet.currency(), withdrawalCurrency);
+        //1. removed call to com/easypeasy/wallet/exchange/model/Wallet::currency → KILLED
+        //NOTE: because of this mutation we had to focus on a case when fromCurrency is the same as toCurrency,
+        BigDecimal withdrawalAmountInWalletCurrency = convertToCurrency(amountToWithdraw, wallet.currency(), withdrawalCurrency);
         // take our fees
         withdrawalAmountInWalletCurrency = withdrawalAmountInWalletCurrency.multiply(
-                BigDecimal.valueOf(1 + calculateFees(userProfile)/100));
+                BigDecimal.valueOf(1 + calculateFees(userProfile, wallet.currency(), withdrawalCurrency)/100));
+        // NOTE: an additional test has shown that we forgot to add rounding to the final multiplication
         walletDao.updateBalanceOnWallet(walletId,
-                BigDecimal.valueOf(wallet.balance()).subtract(withdrawalAmountInWalletCurrency));
+                BigDecimal.valueOf(wallet.balance()).subtract(roundToTheCent(withdrawalAmountInWalletCurrency)));
     }
     private static final double DEFAULT_TRANSACTION_FEE_IN_PERCENT = 2;
     private static final double DEFAULT_EXCHANGE_FEE_IN_PERCENT = 3;
-    private double calculateFees(UserProfile userProfile){
+    private double calculateFees(UserProfile userProfile, Currency fromCurrency, Currency toCurrency){
         double transactionFee = DEFAULT_TRANSACTION_FEE_IN_PERCENT;
         if(userProfile.premiumStatus()){
             transactionFee = 0;
         }
-        return transactionFee + DEFAULT_EXCHANGE_FEE_IN_PERCENT;
+        // NOTE: mutant "removed call to com/easypeasy/wallet/exchange/model/Wallet::currency → KILLED"
+        // helped to discover the bug: we should take exchangeFee only when the exchange was performed.
+        double exchangeFee = DEFAULT_EXCHANGE_FEE_IN_PERCENT;
+        if(fromCurrency == toCurrency){
+            exchangeFee = 0;
+        }
+        return transactionFee + exchangeFee;
     }
+
     // Round down to the nearest cent (two decimal places)
-    private static void roundToTheCent(BigDecimal value){
-        //1. Substituted 2 with 3 → SURVIVED
-        //2. removed call to java/math/BigDecimal::setScale → SURVIVED
-        //3. replaced call to java/math/BigDecimal::setScale with receiver → SURVIVED
-        value.setScale(2, RoundingMode.DOWN);
+    private static BigDecimal roundToTheCent(BigDecimal value){
+        //1. Substituted 2 with 3 → KILLED
+        //2. removed call to java/math/BigDecimal::setScale → KILLED
+        //3. replaced call to java/math/BigDecimal::setScale with receiver → KILLED
+        //NOTE: mutants pointed out to the bug: BigDecimal is immutable, we shouldn't ignore the return
+        return value.setScale(2, RoundingMode.DOWN);
     }
 
     private BigDecimal convertToCurrency(double amountToWithdraw, Currency fromCurrency, Currency toCurrency){
         BigDecimal amount = BigDecimal.valueOf(amountToWithdraw);
-        // replaced equality check with true → SURVIVED
+        // replaced equality check with true → KILLED
         if (fromCurrency != toCurrency) {
-            // removed call to java/time/LocalDateTime::now → SURVIVED
-            double rate = currencyExchange.exchangeRate(toCurrency, fromCurrency, LocalDateTime.now());
+            // removed call to java/time/LocalDateTime::now → KILLED
+            // NOTE: we found a bug here: first and second parameters were swapped
+            double rate = currencyExchange.exchangeRate(fromCurrency, toCurrency, LocalDateTime.now());
             amount = amount.multiply(BigDecimal.valueOf(rate));
-            // removed call to com/easypeasy/wallet/exchange/services/WalletOperationsImpl::roundToTheCent → SURVIVED
-            roundToTheCent(amount);
+            // removed call to com/easypeasy/wallet/exchange/services/WalletOperationsImpl::roundToTheCent → KILLED
+            // NOTE: we found a bug here: function's return was ignored.
+            return roundToTheCent(amount);
         }
         return amount;
     }
--- src/test/java/com/easypeasy/WalletOperationsTest.java
+++ src/test/java/com/easypeasy/WalletOperationsTest.java
@@ -9,6 +9,8 @@ import com.easypeasy.wallet.exchange.model.Currency;
 import com.easypeasy.wallet.exchange.model.UserProfile;
 import com.easypeasy.wallet.exchange.model.Wallet;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,8 +28,12 @@ class WalletOperationsTest {
     void withdraw_userCannotRequestNegativeAmount() {
         when(mockUserDao.getUserProfile(anyString()))
                 .thenReturn(new UserProfile("id123", "user@gmail.com", Set.of(111111L), false));
-
-        assertThrows(IllegalArgumentException.class, () -> underTest.withdraw("id123", -4, Currency.USD, 11111L));
+        when(mockWalletDao.getWallet(anyString())).thenReturn(new Wallet("111111", 100, Currency.EURO));
+        // NOTE: multiple survived mutants has shown that this test should be enhanced:
+        // added proper wallet, so we will now for sure that the first IllegalArgumentException is thrown (not the second validation failed)
+        assertThrows(IllegalArgumentException.class, () -> underTest.withdraw("id123", -4, Currency.USD, 111111L));
+        assertThrows(IllegalArgumentException.class, () -> underTest.withdraw("id123", 0.0, Currency.USD, 111111L));
+        underTest.withdraw("id123", 0.5, Currency.USD, 111111L);
     }
 
     @Test
@@ -56,7 +62,7 @@ class WalletOperationsTest {
         underTest.withdraw("id123", 100, Currency.EURO, 111111L);
         // currency rate is applied: 100 * 1.1 = 110
         // currency exchange operational fee is 3%. 110 * 1.03 = 113.3
-        // 200 - 113.3 = 86.7 usd is left after 100 euro withdrawal
+        // remainder (balance left): 200 - 113.3 = 86.7 USD
         verify(mockWalletDao).updateBalanceOnWallet(eq(111111L), argThat(v -> 86.7 == v.doubleValue()));
     }
 
@@ -71,7 +77,52 @@ class WalletOperationsTest {
         underTest.withdraw("id123", 100, Currency.EURO, 111111L);
         // currency rate is applied: 100 * 1.1 = 110
         // currency exchange fee is 3% + 2% service fee. 110 * 1.05 = 115.5
-        // 200 - 115.5 = 84.5
+        // remainder (balance left): 200 - 115.5 = 84.5
         verify(mockWalletDao).updateBalanceOnWallet(eq(111111L), argThat(v -> 84.5 == v.doubleValue()));
     }
+
+
+    @Test
+    void withdraw_exchangeProviderIsCalledForCurrentDate() {
+        when(mockUserDao.getUserProfile(anyString()))
+                .thenReturn(new UserProfile("id123", "user@gmail.com", Set.of(111111L), true));
+        when(mockWalletDao.getWallet(anyString())).thenReturn(new Wallet("1111111", 100, Currency.EURO));
+
+        when(mockExchange.exchangeRate(eq(Currency.EURO), eq(Currency.USD), any())).thenReturn(1.1);
+        underTest.withdraw("id123", 100, Currency.USD, 111111L);
+
+        verify(mockExchange).exchangeRate(eq(Currency.EURO), eq(Currency.USD),
+                argThat(v -> LocalDate.now().equals(v.toLocalDate())));
+    }
+
+    @Test
+    void withdraw_afterOperations_amountIsRoundedDownToCents() {
+        when(mockUserDao.getUserProfile(anyString()))
+                .thenReturn(new UserProfile("id123", "user@gmail.com", Set.of(111111L), false));
+        when(mockWalletDao.getWallet(anyString())).thenReturn(new Wallet("1111111", 200, Currency.EURO));
+        when(mockExchange.exchangeRate(eq(Currency.EURO), eq(Currency.USD), any())).thenReturn(0.99999);
+
+        underTest.withdraw("id123", 100, Currency.USD, 111111L);
+        // convert to wallet's currency: 100 * 0.99999 = 99.999
+        // rounded down to 99.99
+        // currency exchange operational fee is 3% and service fee is 2%
+        // 99.99 * 1.05 = 104.9895
+        // rounded down to 104.98
+        // remainder (balance left): 200 - 104.98 = 95.02
+        verify(mockWalletDao).updateBalanceOnWallet(eq(111111L), argThat(v -> 95.02 == v.doubleValue()));
+    }
+
+    @Test
+    void withdraw_whenPremiumUserAndTheSameCurrency_noFeeIsTaken() {
+        when(mockUserDao.getUserProfile(anyString()))
+                .thenReturn(new UserProfile("id123", "user@gmail.com", Set.of(111111L), true));
+        when(mockWalletDao.getWallet(anyString())).thenReturn(new Wallet("1111111", 100, Currency.EURO));
+
+        underTest.withdraw("id123", 100, Currency.EURO, 111111L);
+
+        verify(mockWalletDao).updateBalanceOnWallet(eq(111111L), argThat(v -> 0.0 == v.doubleValue()));
+        verify(mockExchange, never()).exchangeRate(any(), any(), any());
+    }
+
+
 }
