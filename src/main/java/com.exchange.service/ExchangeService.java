7e4e297167800dcc0d35b9eb95c1da1c1ae32d90
e1-one <vanya.yarovoy@gmail.com>
2024-03-18 15:10:40 -0400

    added survived mutants from the report to the source code.

--- src/main/java/com.exchange.service/ExchangeService.java
+++ src/main/java/com.exchange.service/ExchangeService.java
@@ -9,6 +9,7 @@ import java.time.LocalDateTime;
 
 @AllArgsConstructor
 public class ExchangeService {
+
     public interface ExchangeRateProvider {
         /**
          * accepts two Currency codes (ISO 4217)
@@ -22,15 +23,23 @@ public class ExchangeService {
     private ExchangeRateProvider exchangeRateProvider;
 
     public double exchange(double amount, Currency givenCurrency, Currency toCurrency){
+        // Substituted 0.0 with 1.0 → SURVIVED
+        // Changed conditional boundary → SURVIVED
         if(amount < 0){
             throw new IllegalArgumentException("Money amount can't be negative value");
         }
         BigDecimal givenMoney = BigDecimal.valueOf(amount);
+        // removed call to java/sql/Timestamp::getTime → SURVIVED
         long currentTime = Timestamp.valueOf(LocalDateTime.now()).getTime();
+        // removed call to com/exchange/service/Currency::getCode → SURVIVED
+        // removed call to com/exchange/service/Currency::getCode → SURVIVED
         double rate = exchangeRateProvider.rate(toCurrency.getCode(), givenCurrency.getCode(),
                 currentTime);
         BigDecimal convertedAmount = givenMoney.multiply(BigDecimal.valueOf(rate));
         //round down to cents
+        // Substituted 2 with 3 → SURVIVED
+        // Removed call to java/math/BigDecimal::setScale → SURVIVED
+        // replaced call to java/math/BigDecimal::setScale with receiver → SURVIVED
         convertedAmount.setScale(2, RoundingMode.DOWN);
         return convertedAmount.doubleValue();
     }
